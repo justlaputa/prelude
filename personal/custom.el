2b8dabf20693f1c72abcd987af5b8ebe49391cbe
Xiao HAN <han_x@worksap.co.jp>
2014-11-05 12:06:03 +0900

    set indent for js2-mode

--- personal/custom.el
+++ personal/custom.el
@@ -8,7 +8,12 @@
 
 (setq speedbar-show-unknown-files t)
 
-(setq js-indent-level 2)
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+
+(setq js2-basic-offset 2)
+(setq js2-bounce-indent-p t)
+(setq js2-highlight-level 3)
 
 (setq org-agenda-files (list "~/.org/work.org"
                              "~/.org/hack.org"
