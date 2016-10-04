e022e12846ff43f100bb8bdc1b6aa61addffff1f
Xiao HAN <han_x@worksap.co.jp>
2016-10-04 19:29:36 +0900

    add view-mode read only in custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -78,16 +78,11 @@ Version 2015-01-26"
 
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 
+(setq view-read-only t)
+
 (provide 'custom)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(tab-width 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  '(tab-width 4))
+
+;;; custom.el ends here
