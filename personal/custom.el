38b86f56269a2b8c3a5b4878e1818ce7b7c39440
Xiao HAN <justlaputa@gmail.com>
2016-11-06 12:24:43 +0900

    add packages to custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -85,9 +85,13 @@ Version 2015-01-26"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default)))
+ '(linum-format " %7i ")
  '(package-selected-packages
    (quote
-    (helm-projectile helm counsel swiper ivy zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu ace-window)))
+    (helm-projectile helm counsel swiper ivy dockerfile-mode go-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree projectile  sublime-themes smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters ov operate-on-number move-text markdown-mode magit lua-mode json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux counsel company browse-kill-ring beacon anzu ace-window)))
  '(tab-width 4))
 
 ;;; custom.el ends here
